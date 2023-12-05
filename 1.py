efd7c94df5b4ac767ab8a0fa29c18a6d40da3488
cc-z <152991415+Cc-zed@users.noreply.github.com>
2023-12-05 19:33:40 +0200

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,44 @@
+# -----------------------------Task 1------------------------------------
+
+# x = int(input("Enter number one: "))
+# y = int(input("Enter number two: "))
+# z = int(input("Enter number three: "))
+# print("Sum: ", x+y+z)
+# print("Res of mnozennya: ", x*y*z)
+
+
+
+# Comment to task 1:
+#           <------- Easy one.
+#           <------- Also i forgot how mnozennya will be on English
+
+
+
+# -----------------------------Task 2------------------------------------
+
+# x = int(input("Enter your salary: "))
+# y = int(input("Enter your monthly credit payment: "))
+# z = int(input("Enter your communal debt: "))
+# print("Money left: ", x-(y+z))
+
+
+
+# Comment to task 2:
+#           <------- Easyy
+
+
+
+# -----------------------------Task 3------------------------------------
+
+# x = int(input("Enter diagonal 1: "))
+# y = int(input("Enter diagonal 2: "))
+# print("area of a rhombus: ", (x*y)/2)
+
+
+
+# Comment to task 2:
+#           <------- Never knew this formula :p
+
+
+
+# ------------------------------END---------------------------------------
---
+++ 2.py
@@ -0,0 +1,106 @@
+# -----------------------------Task 1------------------------------------
+
+
+#        VARIANT 1
+
+
+# x = int(input("Enter number one: "))
+# y = int(input("Enter number two: "))
+# z = int(input("Enter number three: "))
+
+# print("Result: ", x,y,z)
+
+
+
+# Question to task 1 (VARIANT 1): 
+#           <------- Is spaces need to be here, between digits? 
+
+# Comment to task 1 (VARIANT 1):
+#           <------- If not, then there is variant 2
+
+
+
+#        VARIANT 2
+
+
+# x = int(input("Enter number one: "))
+# y = int(input("Enter number two: "))
+# z = int(input("Enter number three: "))
+
+# # Створення числа і виведення результату
+# res = x * 100 + y * 10 + z
+# print("Result: ", res)
+
+
+
+# Comment to task 1 (VARIANT 2): 
+#           <------- But this only works with digits, not numbers.
+#           <------- First one works with numbers and with digits.
+
+
+
+# -----------------------------Task 2------------------------------------
+
+# x = int(input("Enter number: "))
+
+# y = 1
+# for digit in str(x):
+#     y *= int(digit)
+
+# print("Result: ", y)
+
+
+
+# Comment to task 2: 
+#           <------- This works ok, i tested it only with *= and +=
+#           <------- But it should also work with -= and /=
+
+
+
+# -----------------------------Task 3------------------------------------
+
+# x = int(input("Enter number: "))
+
+# print("Cantimeters: ", x*100)
+# print("Dm: ", x*10)
+# print("Milimeters: ", x*1000)
+# print("Miles: ", x*0.000621371)
+
+
+
+# Comment to task 3: 
+#           <------- I think miles is correct... At least i hope.
+#           <------- I googled how much miles in meters, and it said this.
+
+
+
+# -----------------------------Task 4------------------------------------
+
+# x = int(input("Enter base: "))
+# y = int(input("Enter height: "))
+
+# print("Area of triangle: ", (x*y)/2)
+
+
+
+# Comment to task 4: 
+#           <------- Easy one i guess
+
+
+
+# -----------------------------Task 5------------------------------------
+
+# x = int(input("Enter your number: "))
+
+# y = int(str(x)[::-1])
+
+# print("Result: ", y)
+
+
+
+# Comment to task 5: 
+#           <------- Needed time to remember how write digits reversed
+
+
+
+# ------------------------------END---------------------------------------
